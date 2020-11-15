dd0abef81bffdca1ec2f3825b06ef0c94ea00901
DK2554 <ldu0215@gmail.com>
2020-11-15 15:36:35 +0900

    상세페이지

---
+++ src/components/portfolio/PortFolioinfo.vue
@@ -0,0 +1,41 @@
+<template>
+<b-container fluid>
+      <b-row >
+         <div class="submenuimage ">
+        <p class="subtitle" id="subtitle">portfolioinfo</p>
+      </div>
+      </b-row>
+      <b-row>
+        <b-col>제목</b-col>      
+    </b-row>
+
+    작성자
+    내용
+    첨부파일
+    답글     
+</b-container> 
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#subtitle{
+font-family: 'Barlow Semi Condensed', sans-serif;
+}
+.submenuimage{
+  width: 100%;
+  height:180px;
+  background-color:#ffffff;
+  text-align: center;
+  line-height: 180px; 
+}
+.subtitle{
+  font-family: 'Masque';
+  color:#4e5157 ;
+  font-size: 50px;
+}
+</style>
