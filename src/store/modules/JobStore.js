6e73c1bbc71ec9981860a4a790aaf365a9a5fb80
hyeonjeong lee <hlee220@myseneca.ca>
2020-12-08 23:47:33 +0900

    카카오 api수정(지도,로그인)

--- src/store/modules/JobStore.js
+++ src/store/modules/JobStore.js
@@ -28,19 +28,18 @@ const jobStore = {
                 memberPosition.memberPosition
             ) //추천 채용정보
                 .then((response) => {
+
                     var xml = response.data;
                     var json = convert.xml2json(xml, { compact: true });
                     this.rcmJobs = JSON.parse(json);
                     console.log("created!");
-                    console.log(this.rcmJson.wantedRoot.wanted);
+                    console.log(this.rcmJobs);
                     commit('SET_RCM_JOB', this.rcmJobs);
                 });
         },
         async loadXml({ commit }) {
             //최신 채용 정보 xml
             console.log("11")
-
-
             await axios.get("http://openapi.work.go.kr/opi/opi/opia/wantedApi.do?authKey=WNKH0840HVI0HM49CADKA2VR1HJ&callTp=L&returnType=XML&startPage=1&display=20&occupation=214200|214201|214202|214302|022|023|024|025|056")
                 .then((response) => {
 
@@ -60,15 +59,16 @@ const jobStore = {
         },
 
         //상세페이지 
-        loadJobDetail({ commit }, wantedNo) {
-            console.log("상세페이지", wantedNo)
-            axios.get(
+        async loadJobDetail({ commit }, wantedNo) {
+            console.log("상세페이지 22", wantedNo)
+            await axios.get(
                 "http://openapi.work.go.kr/opi/opi/opia/wantedApi.do?authKey=WNKH0840HVI0HM49CADKA2VR1HJ&callTp=D&returnType=XML&infoSvc=VALIDATION&wantedAuthNo=" +
                 wantedNo.wantedNo
             )
                 .then((response) => {
-
+                    console.log("333")
                     var xml = response.data;
+
                     var json = convert.xml2json(xml, { compact: true });
                     this.items = JSON.parse(json);
 
