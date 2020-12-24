5abd29373228242421b5604b38617018d4093ac1
DK2554 <ldu0215@gmail.com>
2020-12-24 12:15:38 +0900

    수정

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 module.exports = {
   publicPath: "/itjobgo/resources/static",
+  
   outputDir: path.resolve(__dirname, "../" + "main/resources/static"),
   "transpileDependencies": [
     "vuetify"
